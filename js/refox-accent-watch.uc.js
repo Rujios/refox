468dadb6905eae185a6dd968d35a555667d42339
Liam Doge <liam@batbox.dev>
2026-07-27 01:10:42 -0500

    Update refox-accent-watch.uc.js

--- js/refox-accent-watch.uc.js
+++ js/refox-accent-watch.uc.js
@@ -62,6 +62,7 @@ toolbar .toolbarbutton-1 > .toolbarbutton-icon {
 #unified-extensions-button .toolbarbutton-icon {
   fill: var(--zen-accent-color10) !important;
   color: var(--zen-accent-color10) !important;
+  --toolbarbutton-icon-fill: var(--zen-accent-color10) !important;
 }
 `;
 
